8d38f919b35691f6add138eb65d4d670382ed320
Ali <alimartakoush@gmail.com>
2025-04-15 12:38:41 -0400

    added pagination

--- store_backend/core/settings.py
+++ store_backend/core/settings.py
@@ -162,7 +162,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
-      'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+      'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+      'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+       'PAGE_SIZE': 10,
 }
 
 SPECTACULAR_SETTINGS={
