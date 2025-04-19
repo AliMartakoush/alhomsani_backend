79956b64ba85b8bd84c69f6d9c5d41792137c345
Ali <alimartakoush@gmail.com>
2025-04-19 10:50:59 -0400

    modified reviews so it enables stalking

--- store_backend/testing.py
+++ store_backend/testing.py
@@ -1,15 +1,13 @@
-def clock(time):
-    dt='AM'
-    hrs,mins=time.split(':')
-    hrs=int(hrs)
-    mins=int(mins)
-    if hrs>=12:
-        de='PM'
-    hrs=hrs%12
-    print(f"0{hrs}:{mins} {dt}")
+n=input()
+x=input()
+a=x.split(" ")
+for i in range(len(a)):
+    a[i]=int(a[i])
 
-
-clo="12:12"
-clock(clo)
-
-
+for i in range(len(a)):
+    if a[i]>0:
+        a[i]=1
+    elif a[i]<0:
+        a[i]=2
+for i in a:
+    print(i,end="")
